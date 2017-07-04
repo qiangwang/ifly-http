3eff29d5fa5d367269c899f41ff9a0986d0b5b7d
qiangwang <wangqiang@baixing.com>
2017-07-04 20:35:50 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM openjdk:7-jdk
+
+RUN wget http://mirrors.163.com/.help/sources.list.jessie -O /etc/apt/sources.list \
+    && apt-get update
+
+RUN apt-get install -y --no-install-recommends ffmpeg
+
+RUN curl -sL https://deb.nodesource.com/setup_8.x | bash - \
+    && apt-get install -y --no-install-recommends nodejs
+
+RUN apt-get install -y --no-install-recommends zip unzip
+
+ENV GRADLE_VERSION 3.4
+RUN mkdir /opt/gradle \
+    && wget https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip \
+    && unzip -d /opt/gradle gradle-${GRADLE_VERSION}-bin.zip \
+    && ln -s /opt/gradle/gradle-${GRADLE_VERSION}/bin/gradle /bin/gradle \
+    && rm gradle-${GRADLE_VERSION}-bin.zip
+
+RUN mkdir /opt/ifly-cli \
+    && wget https://github.com/qiangwang/ifly-cli/archive/v1.0.zip \
+    && unzip v1.0.zip \
+    && rm v1.0.zip \
+    && cd ifly-cli-1.0 \
+    && gradle build \
+    && cp build/libs/ifly-cli.jar /opt/ifly-cli/Cli.jar \
+    && cp -r binLib /opt/ifly-cli \
+    && cd .. \
+    && rm -r ifly-cli-1.0
+
+COPY . /data/app/
+
+WORKDIR /data/app/
---
+++ index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+var shell = require('shelljs');
+var md5 = require('md5');
+var minimist = require('minimist')
+
+var argv = minimist(process.argv.slice(2));
+if (!argv.appId) {
+    throw 'Usage: index.js --appId 讯飞语音appId'
+}
+
+var exec = function (cmd) {
+    return new Promise(function (resolve, reject) {
+        shell.exec(cmd.source, function (code, stdout, stderr) {
+            if (code != 0) {
+                reject(cmd);
+                return;
+            }
+
+            resolve(stdout);
+        });
+    });
+};
+
+app.get('/audioToText', function (req, res) {
+    var audioUrl = req.query.audioUrl;
+    if (!audioUrl || audioUrl.length == 0) {
+        res.json({error: '音频地址不能为空'});
+        return;
+    }
+
+    var audioId = md5(audioUrl);
+
+    var audioPath = '/tmp/' + audioId;
+    var wavPath = '/tmp/' + audioId + '.wav';
+    var textPath = '/tmp/' + audioId + '.txt';
+
+    var cmds = {
+        download: {
+            name: '音频下载',
+            source: 'curl "' + audioUrl + '" -o ' + audioPath
+        },
+        toWav: {
+            name: '音频转码',
+            source: 'ffmpeg -i ' + audioPath + ' -ac 1 -ar 16000 -acodec pcm_s16le -y ' + wavPath
+        },
+        toText: {
+            name: '音频听写',
+            source: 'java -Djava.library.path="/opt/ifly-cli/binLib" -jar /opt/ifly-cli/Cli.jar audioToText --appId ' + argv.appId + ' -i ' + wavPath + ' -o ' + textPath 
+        },
+        readText: {
+            name: '文本读取',
+            source: 'cat ' + textPath
+        }
+    };
+
+    exec(cmds.download).then(function (stdout) {
+        return exec(cmds.toWav);
+    }).then(function (stdout) {
+        return exec(cmds.toText);
+    }).then(function (stdout) {
+        return exec(cmds.readText);
+    }).then(function (stdout) {
+        res.json({text: stdout});
+    }).catch(function (cmd) {
+        res.json({error: cmd.name + '失败'});
+    });
+});
+
+app.listen(9000, function () {
+    console.log('listening on port 9000');
+});
